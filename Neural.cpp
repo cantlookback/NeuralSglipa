0ac6edd545767ebb8dceb0df1f3b0b3c47a5f9dd
cantlookback <zinakov22@gmail.com>
2022-04-06 11:56:42 +0700

    first commit

---
+++ Neural.cpp
@@ -0,0 +1,203 @@
+#include "Neural.h"
+
+NeuralNetwork::NeuralNetwork(int m_layers) {
+    network.first = m_layers;
+    weights.resize(m_layers - 1);
+    values.resize(m_layers);
+
+    cout << "Enter the number of neurons on each layer: " << endl;
+    //Setting the number of neurons on each layer
+    for (unsigned i = 0; i <= weights.size(); i++) {
+        cout << "Layer " << i + 1 << ">>>";
+        int neur = 0;
+        cin >> neur;
+        network.second.push_back(neur);
+        values[i].resize(neur);
+    }
+    //Seting the number of weights on each layer
+    for (int i = 0; i < m_layers - 1; i++) {
+        weights[i].resize(network.second[i] * network.second[i + 1]);
+    }
+    
+}
+
+void NeuralNetwork::print() {
+    
+    cout << "----------------------------\n   NUMBER OF LAYERS\n|";
+    cout << network.first;
+    cout << "|\n----------------------------\n   NUMBER OF NEURONS IN EACH LAYER\n|";
+    for (auto neur : network.second) {
+        cout << neur << '|';
+    }
+    cout << "\n----------------------------\n   NUMBER OF WEIGHT LAYERS\n|";
+    cout << weights.size();
+    cout << "|\n----------------------------\n   ALL WEIGHTS\n|";
+    for (auto l : weights) {
+        for (auto w : l) {
+            cout << w << '|';
+        }
+        cout << "\n|";
+    }
+    
+    cout << "----------------------------\n   VALS\n|";
+    for (auto layer : values) {
+        for (auto val : layer) {
+            cout << val << '|';
+        }
+        cout << "\n|";
+    }
+}
+
+double NeuralNetwork::sigm(double arg) {
+    return 1 / (1 + exp(-arg));
+}
+
+double NeuralNetwork::sigm_deriv(double arg) {
+    return arg * (1 - arg);
+}
+
+void NeuralNetwork::feedForward(vector<double>* data) {
+    //Copy data to input layer
+    for (int i = 0; i < values.size(); i++){
+        for (int j = 0; j < values[i].size(); j++){
+            values[i][j] = 0;
+        }
+    }
+    values[0] = *data;
+    //!For each layer, starting with i = 1
+    //!For each neuron from the i layer and beyond
+    //!For each neuron from i-1 layer
+    //!Value of current neuron = SUM of previous layer neurons * appropriate weight
+    //!Then using activation function on our value
+    for (int i = 1; i < network.first; i++) {
+        for (int j = 0; j < network.second[i]; j++) {
+            for (int k = 0; k < network.second[i - 1]; k++) {
+                values[i][j] += values[i - 1][k] * weights[i - 1][k * network.second[i] + j];
+            }
+            values[i][j] = sigm(values[i][j]);
+        }
+    }
+}
+
+void NeuralNetwork::setWeights() {
+    for (unsigned i = 0; i < weights.size(); i++) {
+        for (unsigned j = 0; j < weights[i].size(); j++) {
+            weights[i][j] = 1;//(static_cast<double>(rand()) / RAND_MAX) * 2 - 1;
+        }
+    }
+}
+
+double NeuralNetwork::MSE(vector<double> Ytrue) {
+    double mse = 0;
+    vector<double> Ypred;
+    Ypred.push_back(values[network.first - 1][0]);
+    for (unsigned i = 0; i < Ypred.size(); i++) {
+        mse += pow(Ytrue[i] - Ypred[i], 2);
+    }
+    mse /= Ypred.size();
+    return mse;
+}
+
+
+//TODO: It's working, but smth wrong, need to fix.
+void NeuralNetwork::train(vector<vector<double>>* data, vector<double>* answers) {
+    //*d_X | Clean
+    vector<vector<double>> d_X;
+    //* GRADs | Clean
+    vector<vector<double>> GRADs;
+    //* dW | noClean
+    vector<vector<double>> dW;
+    
+    d_X.resize(network.first);
+    GRADs.resize(network.first - 1);
+    dW.resize(weights.size());
+
+    for (unsigned i = 0; i < d_X.size(); i++) {
+        d_X[i].resize(network.second[i]);
+    }
+    for (int i = 0; i < network.first - 1; i++) {
+        GRADs[i].resize(network.second[i] * network.second[i + 1]);
+    }
+    for (int i = 0; i < dW.size(); i++){
+        dW[i].resize(weights[i].size());
+    }
+
+    double trainRate = 0.7;
+    double alpha = 0.3;
+    
+    for (unsigned epoc = 0; epoc < 100; epoc++) {
+        //cout << epoc;
+        for (unsigned jija = 0; jija < data->size(); jija++) {
+            feedForward(&(*data)[jija]);
+
+            for (unsigned i = 0; i < d_X[network.first - 1].size(); i++) {
+                d_X[network.first - 1][i] = ((*answers)[i] - values[network.first - 1][i]) * sigm_deriv(values[network.first - 1][i]);
+            }
+
+            for (int i = network.first - 2; i >= 0; i--) {
+                for (unsigned j = 0; j < d_X[i].size(); j++) {
+                    for (unsigned k = 0; k < d_X[i + 1].size(); k++) {
+                        d_X[i][j] += d_X[i + 1][k] * weights[i][k * (network.second[i + 1] - 1) + j];
+                    }
+                    d_X[i][j] *= sigm_deriv(values[i][j]);
+                }
+            }
+            
+            for (unsigned i = 0; i < GRADs.size(); i++) {
+                for (unsigned j = 0; j < GRADs[i].size(); j++) {
+                    GRADs[i][j] = values[i][j % values[i].size()] * d_X[i + 1][int(j / values[i].size())];
+                }
+            }
+/*
+            cout << "----------------------------\n   d_X   \n|";
+            for (auto Gs : d_X){
+                for (auto g : Gs){
+                    cout << g << '|';
+                }
+                cout << "\n|";
+            }
+
+            cout << "----------------------------\n   GRADS   \n|";
+            for (auto Gs : GRADs){
+                for (auto g : Gs){
+                    cout << g << '|';
+                }
+                cout << "\n|";
+            }
+*/
+            //G{0,0} = v{0,0} * d{1,0}
+            //G{0,1} = v{0,1} * d{1,0}
+            //G{0,2} = v{0,0} * d{1,1}
+            //G{0,3} = v{0,1} * d{1,1}
+            //G{0,4} = v{0,0} * d{1,2}
+            //G{0,5} = v{0,1} * d{1,2}
+            //G{1,0} = v{1,0} * d{2,0}
+            //G{1,1} = v{1,1} * d{2,0}
+            //G{1,2} = v{1,2} * d{2,0}
+
+            for (unsigned i = 0; i < dW.size(); i++){
+                for (int j = 0; j < dW[i].size(); j++){
+                    dW[i][j] = trainRate * GRADs[i][j] + alpha * dW[i][j];
+                }
+            }
+
+            for (unsigned i = 0; i < weights.size(); i++){
+                for (int j = 0; j < weights[i].size(); j++){
+                    weights[i][j] += dW[i][j];
+                }
+            }
+
+            for (unsigned i = 0; i < d_X.size(); i++){
+                for (unsigned j = 0; j < d_X[i].size(); j++){
+                    d_X[i][j] = 0;
+                }
+            }
+
+            for (unsigned i = 0; i < GRADs.size(); i++){
+                for (unsigned j = 0; j < GRADs[i].size(); j++){
+                    GRADs[i][j] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Neural.h"
+using namespace std;
+
+    vector<vector<double>> datasheet;
+    vector<double> answers;
+    const int N = 5000;
+
+void makeDatasheet(){
+    ifstream fileInput;
+    fileInput.open("C:\\sem4cpp\\neuralNetwork\\datasheet.txt");
+    double data[N][3];
+    double ans[N];
+
+    bool print = 1;
+    while (print){
+        for (int i = 0 ; i < N; i++){
+            for (int j = 0 ; j < 3; j++){
+                fileInput >> data[i][j];
+            }
+        }
+        print = 0;
+    }
+    fileInput.close();
+
+    print = 1;
+    fileInput.open("C:\\sem4cpp\\neuralNetwork\\answers.txt");
+    while(print){
+        for (int i = 0 ; i < N; i++){
+                fileInput >> ans[i];
+        }
+        print = 0;
+    }
+    fileInput.close();
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < 3; j++){
+            datasheet[i][j] = data[i][j];
+        }
+        answers[i] = ans[i];
+    }
+
+}
+
+
+int main() {
+    NeuralNetwork net {3};
+    net.setWeights();
+    datasheet.resize(N);
+    for (int i = 0; i < N; i++){
+        datasheet[i].resize(3);
+    }
+    answers.resize(N);
+    makeDatasheet();
+
+
+    vector<vector<double>> d = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    vector<double> a = {0, 1, 1, 0};
+    net.train(&d, &a);
+    net.print();
+
+    vector<vector<double>> test = {{1, 0}};
+    net.feedForward(&test[0]);
+    net.print();
+    cout << "\n\n" << net.MSE({1});
+
+
+/*
+    vector<double> test1 = {-3.07276, 3.2815, 3.95383};
+    vector<double> test2 = {3.07276, -3.2815, 3.95383};
+    vector<double> test3 = {3.07276, 3.2815, -3.95383};
+    vector<double> test4 = {-3.07276, -3.2815, 3.95383};
+    vector<double> test5 = {3.07276, -3.2815, -3.95383};
+    vector<double> test6 = {-3.07276, 3.2815, -3.95383};
+
+    net.feedForward(&test1);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+    net.feedForward(&test2);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+    net.feedForward(&test3);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+    net.feedForward(&test4);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+    net.feedForward(&test5);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+    net.feedForward(&test6);
+    cout << "\n\n\n";
+    net.print();
+    cout << endl << "MSE >>> " << net.MSE({1}) << "\n\n\n";
+*/
+
+    return 0;
+}
